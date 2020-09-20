713c608446869916ab8aafc8cc40678f56abd4b0
JungJinwoo <48818574+Taller88@users.noreply.github.com>
2020-09-20 20:13:51 +0900

    Swagger를  활용한 API 문서화 작업

--- startsb/src/main/java/startsb/controller/RestBoardApiController.java
+++ startsb/src/main/java/startsb/controller/RestBoardApiController.java
@@ -9,39 +9,48 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import startsb.dto.BoardDto;
 import startsb.service.BoardService;
 
+@Api(description = "게시판 REST API")
 @RestController
 public class RestBoardApiController {
 
 	@Autowired
 	private BoardService boardService;
 	
+	@ApiOperation(value = "게시글 목록 조회")
 	@RequestMapping(value="/api/board", method=RequestMethod.GET)
 	public List<BoardDto> openBoardList() throws Exception{
 		return boardService.selectBoardList();
 	}
 	
+	@ApiOperation(value = "게시글 쓰기")
 	@RequestMapping(value="/api/board/write" ,method=RequestMethod.POST)
 	public void insertBoard(@RequestBody BoardDto board) throws Exception{
 		boardService.insertBoard(board);
 	}
 	
+	@ApiOperation(value = "게시글 상세 목록 조회")
 	@RequestMapping(value="/api/board/{boardIdx}" ,method=RequestMethod.GET)
-	public BoardDto openBoardDetail(@PathVariable("boardIdx") int boardIdx) throws Exception{
+	public BoardDto openBoardDetail(@PathVariable("boardIdx") @ApiParam("게시글 번호")int boardIdx) throws Exception{
 		return boardService.openBoardDetail(boardIdx);
 	}
 	
 	
+	@ApiOperation(value = "게시글 상세 내용 수정")
 	@RequestMapping(value="/api/board/{boardIdx}", method=RequestMethod.PUT)
 	public String updateBoard(@RequestBody BoardDto board) throws Exception{
 		boardService.updateBoard(board);
 		return "redirect:/board";
 	}
 	
+	@ApiOperation(value = "게시글 삭제")
 	@RequestMapping(value="/api/board/{boardIdx}", method=RequestMethod.DELETE)
-	public String updateBoard(@PathVariable("boardIdx")int boardIdx) throws Exception{
+	public String deleteBoard(@PathVariable("boardIdx") @ApiParam("게시글 번호")int boardIdx) throws Exception{
 		boardService.deleteBoard(boardIdx);
 		return "redirect:/board";
 	}
--- startsb/src/main/java/startsb/dto/BoardDto.java
+++ startsb/src/main/java/startsb/dto/BoardDto.java
@@ -2,16 +2,34 @@ package startsb.dto;
 
 import java.time.LocalDateTime;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+@ApiModel(value="BoardDto: 게시글 내용", description = "게시글 내용")
 @Data
 public class BoardDto {
+	@ApiModelProperty(value="게시글 번호")
 	int boardIdx;
+	
+	@ApiModelProperty(value="게시글 제목")
 	String title;
+	
+	@ApiModelProperty(value="게시글 내용")
 	String contents;
+	
+	@ApiModelProperty(value="게시글 조회수")
 	int hitCnt;
+	
+	@ApiModelProperty(value="게시글 작성자")
 	String creatorId;
+	
+	@ApiModelProperty(value="게시글 생성날짜")
 	LocalDateTime createdDatetime;
+	
+	@ApiModelProperty(value="게시글 수정자")
 	String updaterId;
+
+	@ApiModelProperty(value="게시글 수정날짜")
 	LocalDateTime updatedDatetime;
 }
